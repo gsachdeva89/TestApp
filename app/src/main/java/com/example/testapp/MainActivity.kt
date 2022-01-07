8a5140715cc949384739c4af3fb4fafbabf82f06
gsachdeva89 <gaurav.sachdeva@vcommission.com>
2022-01-07 14:50:43 +0530

    Test for Github project

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Test button", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Test button on Github project", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
